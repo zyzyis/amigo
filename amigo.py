a20a316e77e2b92667d84c77266e556274278f3e
zyzyis <zyzyis@gmail.com>
2016-04-18 22:15:00 +0200

    OO refactor

--- amigo.py
+++ amigo.py
@@ -1,45 +1,114 @@
 #!/usr/bin/python
+__author__ = "Si Yin"
+__email__ = "zyzyis@gmail.com"
+__license__ = "MIT"
 
-from PIL import Image
-from datetime import datetime
 import hashlib
 import time
 import os
 import random
 import shutil
 import sys
+from datetime import datetime
+from PIL import Image
 
-PIC_EXTS = { "JPG", "PNG", "JPEG" }
-PIC_DEST = 'Photos'
-MOV_EXTS = { 'MOV', 'M4V', 'MP4' }
-MOV_DEST = 'Movies'
+class Logger(object):
+    """
+    A simple logger.
+    """
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
 
-def log(message):
-    print '%s' % message
+    def prt(self, message, color):
+        """ print """
+        print '%s%s%s' % (color, message, self.ENDC)
 
-"""
-Calculate the md5 of the file.
-"""
-def md5(fname):
-    MAX_READ = 400 * 1024
-    hash_md5 = hashlib.md5()
-    with open(fname, "rb") as f:
-        # a wild guess
-        chunk = f.read(MAX_READ)
-        hash_md5.update(chunk)
-    return hash_md5.hexdigest()
+    def info(self, message):
+        """ info print """
+        self.prt(message, self.HEADER)
 
-"""
-Returns True when the given two files are identical.
-"""
-def is_file_same(src, dest):
-    # todo: can be optimized
-    return md5(src) == md5(dest)
-    
-"""
-Move the item to the ROOT directory with the date.
-"""
+    def fail(self, message):
+        """ failure print """
+        self.prt(message, self.FAIL)
+
+logger = Logger()
+
+class FileUtil(object):
+    """
+    File utilities.
+    """
+    def _md5sum(self, fname):
+        """
+        Calculate the md5 of the file.
+        """
+        CHUNK_SIZE = 400 * 1024
+        hash_md5 = hashlib.md5()
+        with open(fname, "rb") as f:
+            # a wild guess
+            chunk = f.read(CHUNK_SIZE)
+            hash_md5.update(chunk)
+        return hash_md5.hexdigest()
+
+    def is_file_same(self, src, dest):
+        """
+        Returns True when the given two files are identical.
+        """
+        return md5sum(src) == dest(md5sum)
+
+    def move_file(self, file_name, file_path, date, root):
+        """
+        Move file from one path to the other with the given date information.
+        Args:
+        file_name: original file name.
+        file_path: original full file path (include file name).
+        date: date value.
+        root: the destination path where the file is moved to.
+
+        Returns:
+        A move code.
+        """
+        if not isinstance(date, datetime):
+            raise RuntimeError('Given date is not a valid datetime type value.')
+
+        month = '%02d' % date.month
+        year = '%02d' % date.year
+        day = '%02d' % date.day
+        dest_path = os.path.join(root, year, month, day)
+
+        # creates new folder
+        if not os.path.exists(dest_path):
+            logger.info('Create folder %s' % dest_path)
+            os.makedirs(dest_path)
+
+        # check whether file exists
+        new_file_path = os.path.join(dest_path, file_name)
+        return_code = MoveCode.SUCCESS
+        if os.path.isfile(new_file_path):
+            if self.is_file_same(file_path, new_file_path):
+                logger.info('File %s already exists in %s, ignore' % (file_path, new_file_path))
+                return MoveCode.DUPLICATE
+            else:
+                # generate a new file name with random suffix
+                suffix = random.randint(0, 1 << 16)
+                dest_path = os.path.join(dest_path, '%s_%d' % (file_name, suffix))
+                return_code = MoveCode.SAME_NAME
+        try:
+            shutil.move(file_path, dest_path)
+        except Exception as ex:
+            logger.fail('File %s is failed to move', file_path)
+            return_code = MoveCode.FAILED
+        return return_code
+
 class MoveCode(object):
+    """
+    Move the item to the ROOT directory with the date.
+    """
     SUCCESS = 0
     DUPLICATE = 1
     SAME_NAME = 2
@@ -47,156 +116,152 @@ class MoveCode(object):
     @staticmethod
     def get_size():
         return 4
-    
-def move_file(file_name, file_path, date, root):
-    if not isinstance(date, datetime):
-        raise RuntimeError('Given date is not a valid datetime type value.')
 
-    month = '%02d' % date.month
-    year = '%02d' % date.year
-    day = '%02d' % date.day
-    dest_path = os.path.join(root, year, month, day)
+class Amigo(object):
+    """
+    The Main class contains the business logic.
+    """
+    def __init__(self, pic_root='Photos', mov_root='Movies'):
+        self.pic_root = pic_root
+        self.mov_root = mov_root
+        self.pic_exts = {"JPG", "PNG", "JPEG"}
+        self.mov_exts = {'MOV', 'M4V', 'MP4'}
 
-    # creates new folder
-    if not os.path.exists(dest_path):
-        log('Create folder %s' % dest_path)
-        os.makedirs(dest_path)
-        
-    # check whether file exists
-    new_file_path = os.path.join(dest_path, file_name)
-    return_code = MoveCode.SUCCESS
-    if os.path.isfile(new_file_path):
-        if is_file_same(file_path, new_file_path):
-            log('File %s already exists in %s, ignore' % (file_path, new_file_path))
-            return MoveCode.DUPLICATE
-        else:
-            # generate a new file name with random suffix
-            suffix = random.randint(0, 1 << 16)
-            dest_path = os.path.join(dest_path, '%s_%d' % (file_name, suffix))
-            return_code = MoveCode.SAME_NAME
-    try:        
-        shutil.move(file_path, dest_path)
-    except Exception as ex:
-        log('File %s is failed to move', file_path)
-        return_code = MoveCode.FAILED
-    return return_code
+        # files which are not images nor movies.
+        self.non_imgs = []
+        # files which cannot be moved.
+        self.broken_imgs = []
+        # images which do not contain date information
+        self.empty_date_imgs = []
+        # movies which do not contain date information
+        self.empty_date_movs = []
+        # total images count
+        self.img_count = 0
+        # total movie count
+        self.mov_count = 0
+        # move statistics
+        self.move_count = [0] * MoveCode.get_size()
 
-"""
-Return True when the filename is a picture.
-"""
-def is_a_pic(name):
-    if len(name.split('.')) < 2:
+    def is_a_pic(self, name):
+        """
+        Return True when the filename is a picture.
+        """
+        if len(name.split('.')) < 2:
+            return False
+        ext = name.split('.')[-1]
+        return ext.upper() in self.pic_exts
+
+    def is_a_movie(self, name):
+        """
+        Return True when the filename is a movie.
+        """
+        if len(name.split('.')) < 2:
+            return False
+        ext = name.split('.')[-1]
+        return ext.upper() in self.mov_exts
+
+    def is_valid_item(self, name):
+        """
+        Return True when the item is a valid photo or movie.
+        """
+        # hidden file
+        if name.startswith("._"):
+            return False
+        if self.is_a_pic(name) or self.is_a_movie(name):
+            return True
         return False
-    ext = name.split('.')[-1]
-    return ext.upper() in PIC_EXTS
 
-"""
-Return True when the filename is a movie.
-"""
-def is_a_movie(name):
-    if len(name.split('.')) < 2:
-        return False
-    ext = name.split('.')[-1]
-    return ext.upper() in MOV_EXTS
+    def get_image_date(self, filepath):
+        """
+        Returns the date when the given picture is captured. Returns None when the capture date
+        does not exist in the original file.
+        """
+        KEY = 36867
+        result = None
+        try:
+            with Image.open(file_path) as img:
+                exif = img._getexif()
+                # check the existence of the date
+                if exif.has_key(KEY) and exif[KEY] is not None:
+                    # convert to datetime
+                    value = exif[KEY][0]
+                    result = datetime.strptime(value, '%Y:%m:%d %H:%M:%S')
+        except Exception as ex:
+            logger.fail("Open Image {0} failed {1}".format(file_path, ex.message))
+        return result
 
-""" 
-Return True when the item is a valid photo or movie.
-"""
-def is_valid_item(name):
-    # hidden file
-    if name.startswith("._"):
-        return False
-    if is_a_pic(name) or is_a_movie(name):
-        return True
-    return False
+    def get_movie_date(self, filepath):
+        """
+        Returns the date when the given movie is captured. Returns None when the capture date
+        does not exist in the original file.
+        """
+        return None
 
-"""
-Returns the date when the given picture is captured. Returns None when the capture date
-does not exist in the original file.
-"""
-def get_image_date(filepath):
-    KEY = 36867
-    result = None
-    try:
-        with Image.open(file_path) as img:
-            exif = img._getexif()
-            # check the existence of the date 
-            if exif.has_key(KEY) and exif[KEY] is not None:
-                # convert to datetime
-                value = exif[KEY][0]
-                result = datetime.strptime(value, '%Y:%m:%d %H:%M:%S')
-    except Exception as ex:
-        log("Open Image {0} failed {1}".format(file_path, ex.message))
-    return result
+    def get_file_date(self, filepath):
+        """
+        Returns the date of the file creation.
+        """
+        time_value = time.ctime(os.path.getmtime(filepath))
+        return datetime.strptime(time_value, "%a %b %d %H:%M:%S %Y")
 
-"""
-Returns the date when the given movie is captured. Returns None when the capture date
-does not exist in the original file.
-"""
-def get_movie_date(filepath):
-    return None
+    def run(self, src, dst):
+        """
+        Run the movement from source directory to the destination directory.
+        """
+        for root, dirs, files in os.walk(dirname):
+            for name in files:
+                file_path = os.path.join(root, name)
+                # check file validation
+                if not self.is_valid_item(file_path):
+                    self.non_imgs.append(file_path)
+                    continue
 
-"""
-Returns the date of the file creation. 
-"""
-def get_file_date(filepath):
-    time_value = time.ctime(os.path.getmtime(filepath))
-    return datetime.strptime(time_value, "%a %b %d %H:%M:%S %Y")
+                dest_path = root_dest
+                date = None
+                if self.is_a_pic(name):
+                    img_count = img_count + 1
+                    date = self.get_image_date(file_path)
+                    dest_path = os.path.join(root_dest, PIC_DEST)
+                    if date is None:
+                        self.empty_date_imgs.append(file_path)
+                elif self.is_a_movie(name):
+                    self.mov_count = self.mov_count + 1
+                    date = self.get_movie_date(file_path)
+                    dest_path = os.path.join(root_dest, MOV_DEST)
+                    if date is None:
+                        self.empty_date_movs.append(file_path)
 
-# Main entry
+                file_util = FileUtil()
+                # if there is no date information, we will use the file date
+                if date is None:
+                    date = file_util.get_file_date(file_path)
+                    # for now, does not know how to get the movie date
+                    if self.is_a_pic(name):
+                        logger.info('Cannot parse the date of %s, use file creation time %s' \
+                            % (file_path, date))
+
+                # move the file
+                rc = file_util.move_file(name, file_path, date, dest_path)
+                self.move_count[rc] = self.move_count[rc] + 1
+
+    def print_result(self):
+        logger.prt('-------------------------', Logger.OKBLUE)
+        logger.prt('Total Images     : %25d' % img_count, Logger.OKBLUE)
+        logger.prt('Empty Date Images: %25d' % len(empty_date_imgs), Logger.OKBLUE)
+        logger.prt('Non Images       : %25d' % len(non_imgs), Logger.OKBLUE)
+        logger.prt('Empty Date Movies: %25d' % len(empty_date_movs), Logger.OKBLUE)
+        logger.prt('Move             : %25d' % move_count[MoveCode.SUCCESS], Logger.OKBLUE)
+        logger.prt('Duplicate        : %25d' % move_count[MoveCode.DUPLICATE], Logger.OKBLUE)
+        logger.prt('Same File Name   : %25d' % move_count[MoveCode.SAME_NAME], Logger.OKBLUE)
+        logger.prt('Failure Count    : %25d' % move_count[MoveCode.FAILED], Logger.OKBLUE)
+
+# Main
 if __name__ == '__main__':
     if len(sys.argv) != 3:
         print "%s <dirname> <image root>" % sys.argv[0]
         sys.exit(1)
 
-    (script, dirname, root_dest) = sys.argv
-    non_imgs = []
-    broken_imgs = []
-    empty_date_imgs = []
-    empty_date_movs = []
-    img_count = 0
-    mov_count = 0
-    move_count = [0] * MoveCode.get_size()
-    
-    for root, dirs, files in os.walk(dirname):
-        for name in files:
-            file_path = os.path.join(root, name)
-            # check file validation
-            if not is_valid_item(file_path):
-                non_imgs.append(file_path)
-                continue
-
-            dest_path = root_dest
-            date = None
-            if is_a_pic(name):
-                img_count = img_count + 1
-                date = get_image_date(file_path)
-                dest_path = os.path.join(root_dest, PIC_DEST)
-                if date is None:
-                    empty_date_imgs.append(file_path)
-            elif is_a_movie(name):
-                mov_count = mov_count + 1
-                date = get_movie_date(file_path)
-                dest_path = os.path.join(root_dest, MOV_DEST)
-                if date is None:
-                    empty_date_movs.append(file_path)
-                
-            # if there is no date information, we will use the file date
-            if date is None:
-                date = get_file_date(file_path)
-                # for now, does not know how to get the movie date
-                if is_a_pic(name):
-                    log('Cannot parse the date of %s, use file creation time %s' % (file_path, date))
-
-            # move the file
-            rc = move_file(name, file_path, date, dest_path)
-            move_count[rc] = move_count[rc] + 1
-    log('-------------------------')
-    log('Total Images:      %25d' % img_count)
-    log('Non Images:        %25d' % len(non_imgs))
-    log('Empty Date Images: %25d' % len(empty_date_imgs))
-    log('Empty Date Movies: %25d' % len(empty_date_movs))
-    log('Move             : %25d' % move_count[MoveCode.SUCCESS])
-    log('Duplicate        : %25d' % move_count[MoveCode.DUPLICATE])
-    log('Same File Name   : %25d' % move_count[MoveCode.SAME_NAME])
-    log('Failure Count    : %25d' % move_count[MoveCode.FAILED])
+    (script, src, dst) = sys.argv
+    amigo = Amigo()
+    amigo.run(src, dst)
+    amigo.print_result()
